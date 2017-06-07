0e3791addc4b5a190daf3ca3dfaa954e2be5faa2
He Xing Darius <E0007888@u.nus.edu>
2017-06-07 10:36:51 +0800

    Use RegEX to remove subnet prefix

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,14 @@ router.get('/', function(req, res) {
   var language = req.headers['accept-language'].split(",")[0]
   var software = req.headers['user-agent'].match(/\((.+?)\)/)[1]
   var ip = requestIP.getClientIp(req)
+  var trimmedIP = req.ip.replace(/\:(.*)\:(.*)\:(.*)\:(.*)/, '')
 
   var result = {'ipaddress' : req.ip ,'Language' : language, 'software': software };
 
   res.json(result);
-  console.log(JSON.stringify(req.headers));
+  //console.log(JSON.stringify(req.headers));
+  console.log(trimmedIP)
+  console.log(req.ip)
 
 });
 module.exports = router;
