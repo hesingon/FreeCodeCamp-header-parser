c5942afbefe891e7adbd77b956952a71e29c8aac
He Xing Darius <E0007888@u.nus.edu>
2017-06-07 21:51:05 +0800

    inlcude x-forwarded-for and another method to display ip

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,19 @@ var express = require("express");
 var router = express.Router();
 var requestIP = require("request-ip")
 
+var app = express()
+app.enable('trust proxy')
+
 /* GET home page. */
 router.get('/', function(req, res) {
 
   var language = req.headers['accept-language'].split(",")[0]
   var software = req.headers['user-agent'].match(/\((.+?)\)/)[1]
   var ip = requestIP.getClientIp(req)
+  var ip2 = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   var trimmedIP = req.ip.replace(/(.*)\:(.*)\:(.*)\:/, '')
 
-  var result = {'ipaddress' : trimmedIP ,'Language' : language, 'software': software };
+  var result = {'ipaddress1' : trimmedIP ,'ipaddress2' : ip2 , 'ipaddress3' : req.ip ,'Language' : language, 'software': software };
 
   res.json(result);
   //console.log(JSON.stringify(req.headers));
